c2364c2801c2e048fb65a850ed1b6906d09afffb
alexcaussades <alexcaussades@gmail.com>
2017-04-17 11:44:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- header su site web-->
+
+<? require 'template/header.php'; ?>
+
+<!-- Fomulaire de recherche-->
+
+<? require 'fonction/googlemodo.php'; ?>
+
+<!-- Formulaire de création de donnes -->
+
+<? require 'fonction/moderation.php'; ?>
+
+<!-- tableau de de la base, en auto refresher "1s" -->
+
+<?php require 'fonction/table.php'; ?>
+
+<!--fonction refresher-->
+
+<?php require 'fonction/refresher.php'; ?>
+
+</html>
