84b23dae1f9eb030cd8371b96323b55c3734b989
alexcaussades <alexcaussades@gmail.com>
2017-04-19 03:11:15 +0200

    Create sql.php

---
+++ fonction/sql.php
@@ -0,0 +1,9 @@
+<?
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=;charset=utf8', '', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+  die("Erreur connection sql");
+}
