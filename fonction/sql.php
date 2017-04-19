0cadc514670a6551b47cb9e943424968499784f1
alexcaussades <alexcaussades@gmail.com>
2017-04-19 03:12:48 +0200

    Update sql.php

--- fonction/sql.php
+++ fonction/sql.php
@@ -1,7 +1,7 @@
 <?
 try
 {
-$bdd = new PDO('mysql:host=localhost;dbname=;charset=utf8', '', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host=localhost;dbname=;charset=utf8', 'root', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exception $e)
 {
